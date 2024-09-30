3e49db15a34aa52fb5b47cd160046686a37f842d
Liza Nadieieva <elizabethnadeevaa@gmail.com>
2024-09-30 14:10:30 +0200

    connecting to db

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+//connecting to db
+//db as url
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindandModify: false
+}).then(con => {
+	console.log('db connecton successul')
+});
 
 const port = process.env.PORT || 3000;
 ``;
