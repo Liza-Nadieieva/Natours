479479b127731cae48c95f10412c1ffd5cee7cea
Liza Nadieieva <elizabethnadeevaa@gmail.com>
2024-12-18 18:07:16 +0100

    implemented maximum login attempts

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router(); //middleware
 
 
 router.post('/signup', authController.signup);
-router.post('/login', authController.checkLock, authController.login);
+router.post('/login', authController.login);
 router.post('/verify2fa', authController.verify2FA);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
