35c6a782c0eafe285a2c2fbc6a1f473eea7641f1
Liza Nadieieva <elizabethnadeevaa@gmail.com>
2025-01-24 15:32:17 +0100

    set up structure

---
+++ routes/viewRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const viewsContoller = require('../controllers/viewsController');
+
+const router = express.Router();
+
+router.get('/', viewsContoller.getOverview)
+router.get('/tour', viewsContoller.getTour)
+
+module.exports = router;
